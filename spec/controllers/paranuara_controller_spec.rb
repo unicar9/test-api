5b99b2a8a65ddefdfaf6c70492266243e2ad1331
Weijia Li <unicar9@gmail.com>
2017-08-24 09:00:36 +1000

    before clone development database

--- spec/controllers/paranuara_controller_spec.rb
+++ spec/controllers/paranuara_controller_spec.rb
@@ -2,11 +2,20 @@ require 'rails_helper'
 
 RSpec.describe ParanuaraController, type: :controller do
 
-  describe "GET #company" do
-    it "returns a success response" do
-      peach = Peach.create! valid_attributes
-      get :company, params: {}, session: valid_session
-      expect(response).to be_success
+  describe "GET /company/:name" do
+    before { get :company, params: { name: "MAINELAND" }}
+
+    context 'when the company exists' do
+      it 'returns the employees of the company' do
+        @company = Company.find_by(company: "MAINELAND")
+        res = JSON.parse( response.body )
+        expect(res).not_to be_empty
+        expect(res['number_of_employees']).to eq (@company.people.length)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
     end
   end
 
